b49dbdd7421e2ff76fb0d4fc3f56739d7563e4b6
Naltpom <nathan.provost78990@gmail.com>
2021-09-02 16:03:32 +0200

    Update dateFormat.js

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -8,7 +8,7 @@ class DateFormat
         if (date instanceof Date && isNaN(date)) {
             throw new Error('Parameter date is not valid Date!')
         }
-        console.log(date)
+
         const days = { 0: 'Dimanche', 1: 'Lundi', 2: 'Mardi', 3: 'Mercredi', 4: 'Jeudi', 5: 'Vendredi', 6: 'Samedi' }
         let day = days[date.getDay()]
 
