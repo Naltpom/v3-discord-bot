65e1a807a0b349c062d7364a9155f19777f5f75f
Nathan PROVOST <nathan.provost78990@gmail.com>
2021-10-02 18:11:40 +0200

    Update create.js

--- src/sequelize/create.js
+++ src/sequelize/create.js
@@ -29,44 +29,6 @@ class Create {
         table.sync();
     }
 
-    
-    static async sqlUsers(table, guild) {
-        await guild.members.fetch().then(u => {
-            u.map(m => {           
-                let rolelist = {};
-                m._roles.map(r => {
-                    let rl = guild.roles.cache.get(r)
-                    rolelist[rl.name.toLowerCase()] = r
-                })
-                table.findOne({where: {_id: m.user.id}}).then(item => {
-                    if (item) {
-                        Logger.log('info',`update ${m.user.username} in DB user`, 'database')
-                        return item.update({
-                            name: m.user.username,
-                            nickname: m.nickname ?? m.user.username,
-                            role: rolelist,
-                            time: Date.now()
-                        }).catch(e =>  Logger.log('error', e.message, 'database'));
-                    } else {
-                        Logger.log('info',`create ${m.user.username} in DB user`, 'database')
-
-                        return table.create({
-                            name: m.user.username,
-                            guild: m.guild.id,
-                            nickname: m.nickname ?? m.user.username,
-                            _id: m.user.id,
-                            slug: m.user.id,
-                            role: rolelist,
-                            time: Date.now(),
-                            slugName: sluger(m.user.username, {lower: true}),
-                        }).catch(e =>  Logger.log('error', e.message, 'database'));
-                    }
-                })
-                .catch(e =>  Logger.log('error', e.message, 'database'));
-            })
-        }).catch(e =>  Logger.log('error', e.message, 'database'))
-    }
-
     static async syncDb(table, list) {
         list.map(item => {
             table.findOne({where: {_id: item._id, guild: item.guild}})
